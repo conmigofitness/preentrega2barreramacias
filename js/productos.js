fa882235821bad5881d1bdbe8166797665365efe
¨David <conmigofitness@gmail.com>
2023-08-10 14:28:39 -0600

    2dapreentregabarreramaciasbrayandavid

---
+++ js/productos.js
@@ -0,0 +1,30 @@
+const paquetes = [
+  {
+    id: 01,
+    tipo: "Paquete",
+    nombre: "Creatina",
+    precio: 450,
+    foto: "./suplementos/creatina-.jpeg",
+  },
+  {
+    id: 02,
+    tipo: "Paquete",
+    nombre: "Hidro Whey",
+    precio: 750,
+    foto: "./suplementos/hydroWhey.jpeg",
+  },
+  {
+    id: 03,
+    tipo: "Paquete",
+    nombre: "Pre Workout",
+    precio: 450,
+    foto: "./suplementos/preWorkout.jpeg",
+  },
+  {
+    id: 04,
+    tipo: "Paquete",
+    nombre: "Aminoacidos",
+    precio: 550,
+    foto: "./suplementos/top1bcaas.jpeg",
+  },
+];
